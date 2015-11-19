ef937f0bf51e4ae2ab09221e986b9e5d0aefcf53
Devin Higgins <higgi135.mail.lib.msu.edu>
2015-11-19 17:31:09 -0500

    Update to python3 and for new format

--- vvlbext.py
+++ vvlbext.py
@@ -1,16 +1,33 @@
 import os
 
-source_file_path = "/Users/higgi135/Projects/vvlbext/vvl_ryan-reconfig-titles.csv"
 problem_records = ["14036", "14041", "14042", "14055", "14061", "14073"]
 
+
 class VvlBext():
+    """Convert data from DBoutput to embed in .wav headers.
+
+    Load MSUL-custom database output into a format suitable for automated
+    transfer to .wav file headers.
+    """
 
     def __init__(self):
+        """Initialize variables."""
         self.record_id = 0
         self.output_csv_data = []
 
 
     def process_csv(self, source_file_path, output_file_path):
+        """Reformat CSV files.
+
+        Take initial CSV and read it in order to collapse identical records
+        from different lines into 1 line; format output data to work with
+        automated tool to add data to .wav headers.
+
+        args:
+            source_file_path(str): CSV file to load, has to match particular
+                                   (peculiar) format.
+            output_file_path(str): any writable file location.
+        """            
 
         with open(source_file_path, "r") as source:
 
@@ -50,6 +67,11 @@ class VvlBext():
 
 
     def get_default_fields(self, line):
+        """Select column data based on line index.
+
+        args:
+            line(str): a line of CSV data, as a string.
+        """
 
         row_items = line.split(",")
         self.record_id = row_items[0].rstrip().replace('"', '').replace("'", "").lstrip()
@@ -60,6 +82,7 @@ class VvlBext():
         self.format_types.append(self.format_type)
 
     def get_addl_fields(self):
+        """Pull out additional fields based on conditionals."""
 
         if self.format_id.startswith("DB") and self.format_type == "wav":
             self.csv_data["digital_wav_path"] = os.path.join(self.location_name.replace("DarkArchive", "archives"), self.format_id + ".wav")
@@ -74,6 +97,17 @@ class VvlBext():
             self.csv_data["analogue_id"] = self.format_id
 
     def new_record(self, line):
+        """Check if each new line ID matches the current record ID.
+
+        If the line doesn't match the record ID established on the previous
+        line, then consider this line to be data about a new record.
+
+        args:
+            line(str): 1 line (as a string) of CSV data.
+
+        returns:
+            (bool): True if the line matches the previous, False if not.
+        """
         
         if line.split(",")[0].rstrip().replace('"', '').replace("'", "").lstrip() == self.record_id:
 
@@ -84,6 +118,7 @@ class VvlBext():
             return True
 
     def create_csv_line(self):
+        """Create line of output CSV data."""
 
         self.output_csv_data.append(",".join([self.record_id, 
                                          self.vvl_number, 
---
+++ vvlbext3.py
@@ -0,0 +1,217 @@
+"""Python 3 version of vvlbext.py."""
+
+import os
+import csv
+
+problem_records = ["14036", "14041", "14042", "14055", "14061", "14073"]
+
+
+class VvlBext():
+    """Convert data from DBoutput to embed in .wav headers.
+
+    Load MSUL-custom database output into a format suitable for automated
+    transfer to .wav file headers.
+    """
+
+    def __init__(self):
+        """Initialize variables."""
+        self.record_id = 0
+        self.output_csv_data = []
+        self.error_records = []
+
+
+    def process_csv(self, source_file_path, output_file_path):
+        """Reformat CSV files.
+
+        Take initial CSV and read it in order to collapse identical records
+        from different lines into 1 line; format output data to work with
+        automated tool to add data to .wav headers.
+
+        args:
+            source_file_path(str): CSV file to load, has to match particular
+                                   (peculiar) format.
+            output_file_path(str): any writable file location.
+        """            
+
+        with open(source_file_path, "r", encoding="latin-1") as csvfile:
+            vvlreader = csv.DictReader(csvfile)
+            for row in vvlreader:
+                if row["record_id"].isdigit():
+                    if int(row["record_id"]) % 1000 == 0:
+                        print("Processed {0} rows".format(row["record_id"]))
+                    if self.new_record(row):
+                        
+                        if self.record_id != 0:
+                            self.create_csv_line()
+                        
+                        self.format_types = []
+                        self.physical_types = []
+                        self.csv_data = {
+                                         "digital_wav_id":"",
+                                         "digital_mp3_id":"",
+                                         "analogue_id":"",
+                                         "digital_wav_path": "",
+                                         "digital_mp3_path": "",
+                                         "source_recording_specs": "",
+                                         "physical_format": "",
+                        }              
+                        self.get_default_fields(row)
+                        self.get_addl_fields()
+                    
+                    else:
+
+                        self.get_default_fields(row)
+                        self.get_addl_fields()
+
+                else:
+
+                    self.error_records.append(row["record_id"])
+
+
+        with open(output_file_path, "w") as csvfile:
+            fieldnames = ["Filename", "Description", "Originator", "CodingHistory",
+                          "IARL", "IART", "ICMT", "ICRD", "IMED", "ISRC", "ISRF"]
+            csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            csvwriter.writeheader()
+            for record in self.output_csv_data:
+                csvwriter.writerow(record)
+            #output.write("record_id,vvl_number,source_format_id,wav_id,mp3_id,mp3_path,FileName,Description,Originator,OriginationDate,CodingHistory,AllFormats\n")
+            #for line in self.output_csv_data:
+            #    output.write(line+"\n")
+
+        for error in self.error_records:
+            print("Error processing row with ID {0}".format(error))
+
+
+    def get_default_fields(self, row):
+        """Select column data based on line index.
+
+        args:
+            line(str): a line of CSV data, as a string.
+        """
+        self.record_id = row["record_id"].rstrip().replace('"', '').replace("'", "").lstrip()
+        self.vvl_number = row["vvl_number"].replace('"', '').replace("'", "").rstrip().lstrip()
+        self.format_id = row["formatid"].replace('"', '').replace("'", "").rstrip().lstrip()
+        self.location_name = row["location_name"].replace('"', '').replace("'", "").rstrip().lstrip()
+        self.format_type = row["formattype_name"].replace('"', '').replace("'", "").rstrip().lstrip()
+        self.main_speaker = row["main_speaker"].strip()
+        self.addl_speakers = row["additional_speakers"].strip()
+        self.summary = row["summary"].strip()
+        self.date_fixed = row["date_fixed"].strip()
+        self.recording_source = row["recording_source"].strip()
+        self.is_physical = row["is_physical"]
+        self.format_types.append(self.format_type)
+
+    def get_addl_fields(self):
+        """Pull out additional fields based on conditionals."""
+
+        if self.format_id.startswith("DB") and self.format_type == "wav":
+            self.csv_data["digital_wav_path"] = os.path.join(self.location_name.replace("DarkArchive", "archives"), self.format_id + ".wav")
+            self.csv_data["digital_wav_id"] = self.format_id
+
+        elif self.format_type == "mp3":
+            self.csv_data["digital_mp3_path"] = os.path.join(self.location_name, self.format_id + ".mp3")
+            self.csv_data["digital_mp3_id"] = self.format_id
+
+        elif not self.format_id.startswith("DB"):
+            self.csv_data["source_recording_specs"] = self.format_type
+            self.csv_data["analogue_id"] = self.format_id
+
+        if self.is_physical == "1":
+            self.physical_types.append(self.format_type)
+            self.csv_data["physical_format"] = self.format_type
+
+
+
+    def new_record(self, row):
+        """Check if each new line ID matches the current record ID.
+
+        If the line doesn't match the record ID established on the previous
+        line, then consider this line to be data about a new record.
+
+        args:
+            line(str): 1 line (as a string) of CSV data.
+
+        returns:
+            (bool): True if the line matches the previous, False if not.
+        """
+        
+        if row["record_id"].rstrip().replace('"', '').replace("'", "").lstrip() == self.record_id:
+
+            return False
+
+        else:
+
+            return True
+
+    def create_csv_line(self):
+        """Create line of output CSV data."""
+
+
+        self.output_row = {"Filename": self.csv_data["digital_wav_path"],
+                           "Description":self.make_description(),
+                           "Originator":"Vincent Voice Library, MSU",
+                           "CodingHistory": self.get_physical_format(),
+                           "IARL": "US, MSU/VVL",
+                           "IART": '; '.join([s for s in [self.main_speaker, 
+                                              self.addl_speakers] if s != ""]),
+                            "ICMT": self.summary,
+                            "ICRD": self.date_fixed,
+                            "IMED": "; ".join(self.format_types),
+                            "ISRC": self.recording_source,
+                            "ISRF": "; ".join(self.physical_types),
+
+                           }
+
+        """
+        self.output_csv_data.append(",".join([self.record_id, 
+                                         self.vvl_number, 
+                                         self.csv_data["analogue_id"],
+                                         self.csv_data["digital_wav_id"],
+                                         self.csv_data["digital_mp3_id"],
+                                         self.csv_data["digital_mp3_path"],
+                                         self.csv_data["digital_wav_path"],
+                                         self.make_description(),
+                                         '"Vincent Voice Library, MSU"',
+                                         "[YYYY-MM-DD]",
+                                         self.csv_data["source_recording_specs"],
+                                         "|".join([f for f in set(self.format_types)])
+                                         ]))
+        """
+        self.output_csv_data.append(self.output_row)
+
+
+    def make_description(self):
+
+        description = ""
+
+        if self.csv_data["analogue_id"] != "":
+            description += "File Content: Collection VVL Item {0}. ".format(self.csv_data["analogue_id"])
+
+        description += "File use: Preservation master. "
+
+        if self.vvl_number != "":
+            description += "Title control number: {0}. ".format(self.vvl_number)
+
+        if self.csv_data["digital_wav_id"] != "":
+            description += "Original filename: {0}.".format(self.csv_data["digital_wav_id"] + ".wav")
+
+        return description
+
+    def get_physical_format(self):
+
+        phys_format = self.csv_data["physical_format"]
+        if  phys_format != "":
+            if phys_format == "cd":
+                return phys_format
+            else:
+                return "A=ANALOG; {0}".format(phys_format)
+
+        else:
+            return "null"
+
+
+
+
+
+
